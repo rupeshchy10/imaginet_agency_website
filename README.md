8ee085a819d8d470da1a4aa20c457cc3817d66a6
rupeshchy10 <rupevilary1010@gmail.com>
2025-11-06 11:54:20 +0545

    full structure design

---
+++ README.md
@@ -0,0 +1,37 @@
+# 🌐 Imaginet — Creative Agency Website
+
+**Imaginet** is a modern, fully responsive digital agency website built using **React**, **Tailwind CSS**, **Framer Motion**, and **Web3Forms**.  
+It showcases the agency’s creative services, portfolio, and contact functionality with smooth animations and an elegant design.
+
+---
+
+## 🚀 Tech Stack
+
+- ⚛️ **React.js** — Frontend library for building dynamic user interfaces  
+- 🎨 **Tailwind CSS** — Utility-first CSS framework for rapid and responsive design  
+- 💫 **Framer Motion** — Animation library for smooth transitions and motion effects  
+- ✉️ **Web3Forms** — Serverless form submission for handling contact forms easily  
+- 🧠 **JavaScript (ES6+)** — Logic and interactivity  
+
+---
+
+
+---
+
+## ⚙️ Installation & Setup
+
+Follow these steps to run the project locally:
+
+```bash
+# 1️⃣ Clone this repository
+git clone https://github.com/rupeshchy10/imaginet_agency_website.git
+
+# 2️⃣ Navigate into the project folder
+cd imaginet
+
+# 3️⃣ Install dependencies
+npm install
+
+# 4️⃣ Start the development server
+npm run dev
+
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,90 @@
+import toast from "react-hot-toast";
+import assets from "../assets/assets";
+import Title from "./Title";
+
+const ContactUs = () => {
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+
+		formData.append("access_key", "c6a17e47-f91f-4f5f-9aca-0eeacf17f955");
+
+		try {
+			const response = await fetch("https://api.web3forms.com/submit", {
+			method: "POST",
+			body: formData,
+		});
+
+		const data = await response.json();
+
+		if (data.success) {
+			toast.success("Thank you for your submission!");
+			event.target.reset();
+		} else {
+			toast.error(data.message);
+		}
+		} catch (error) {
+						toast.error(error.message);
+
+		}
+	};
+	return (
+		<div
+			id="contact-us"
+			className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
+		>
+			<Title
+				title="React out to us"
+				desc="From strategy to execution, we craft digital solutions that move your business forward."
+			/>
+			<form
+				onSubmit={onSubmit}
+				className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full"
+			>
+				<div>
+					<p className="mb-2 text-sm font-medium">Your name</p>
+					<div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+						<img src={assets.person_icon} alt="" className="" />
+						<input
+							type="text"
+							name="name"
+							placeholder="Enter your name"
+							className="w-full p-3 text-sm outline-none"
+							required
+						/>
+					</div>
+				</div>
+				<div>
+					<p className="mb-2 text-sm font-medium">Email Id</p>
+					<div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+						<img src={assets.email_icon} alt="" className="" />
+						<input
+							type="text"
+							name="email"
+							placeholder="Enter your email"
+							className="w-full p-3 text-sm outline-none"
+							required
+						/>
+					</div>
+				</div>
+				<div className="sm:col-span-2">
+					<p className="mb-2 text-sm font-medium">Message</p>
+					<textarea
+						name="message"
+						rows={8}
+						placeholder="Enter your message"
+						className="w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600"
+						required
+					/>
+				</div>
+				<button
+					type="submit"
+					className="w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all"
+				>
+					Submit <img src={assets.arrow_icon} alt="" className="w-4" />
+				</button>
+			</form>
+		</div>
+	);
+};
+export default ContactUs;
