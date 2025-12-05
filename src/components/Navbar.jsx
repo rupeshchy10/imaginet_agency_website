b004193c363ef21d019c49936b74077cf887b88a
rupeshchy10 <rupevilary1010@gmail.com>
2025-12-05 21:25:22 +0545

    Refactor Navbar component and update font-weight in CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,12 @@ const Navbar = ({ theme, setTheme }) => {
 			transition={{ duration: 0.6, ease: "easeOut" }}
 			className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70"
 		>
-			<img
+			{/* <img
 				src={theme === "dark" ? assets.logo_dark : assets.logo}
 				className="w-32 sm:w-40"
 				alt="logo_pic"
-			/>
+			/> */}
+            <div className="text-2xl font-bold dark:text-white">Imagi<span className="text-blue-500">net</span></div>
 
 			<div
 				className={`text-gray-700 dark:text-white sm:text-sm ${
